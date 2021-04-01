29b2a2baf2f96255eb52b8cf04be1ec41e69d499
lexcai <boudeau.axel24@gmail.com>
2021-04-01 16:24:29 +0200

    ajout des info de l'api

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using Newtonsoft.Json;
+using System.Net.Http;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -20,7 +22,25 @@ namespace BridgeMonitor.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var boats = GetBoatsFromApi();
+            return View(boats);
+        }
+
+        private static List<Boat> GetBoatsFromApi()
+        {
+            //Création un HttpClient (= outil qui va permettre d'interroger une URl via une requête HTTP)
+            using (var client = new HttpClient())
+            {
+                //Interrogation de l'URL censée me retourner les données
+                var response = client.GetAsync("https://api.alexandredubois.com/pont-chaban/api.php");
+                //Récupération du corps de la réponse HTTP sous forme de chaîne de caractères
+                var stringResult = response.Result.Content.ReadAsStringAsync();
+                //Conversion de mon flux JSON (string) en une collection d'objets BikeStation
+                //d'un flux de données vers des objets => Déserialisation
+                //d'objets vers un flux de données => Sérialisation
+                var result = JsonConvert.DeserializeObject<List<Boat>>(stringResult.Result);
+                return result;
+            }
         }
 
         public IActionResult Privacy()
@@ -35,3 +55,4 @@ namespace BridgeMonitor.Controllers
         }
     }
 }
+
---
+++ Models/Boat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace BridgeMonitor.Models
+{
+    public class Boat
+    {
+        [JsonProperty("boat_name")]
+        public string BoatName { get; set; }
+        [JsonProperty("closing_type")]
+        public string ClosingType { get; set; }
+        [JsonProperty("closing_date")]
+        public DateTime ClosingDate { get; set; }
+        [JsonProperty("reopening_date")]
+        public DateTime ReopeningDate { get; set; }
+
+    }
+}
